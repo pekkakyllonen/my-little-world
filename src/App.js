9140b5d4120f5f451d484c6e075283ff774bf5c4
pekkakyllonen <pkyllone@gmail.com>
2019-07-02 16:15:21 +0300

    Another branch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default () => (
   <div className="App">
     <header className="App-header">
       <h1 className="App-title">Welcome To My Little World!</h1>
-      <h2>My name is TravisTest. Welcome, and hello!</h2>
+      <h2>My name is TravisTest. Thanks for visiting, and farewell!</h2>
     </header>
   </div>
 );
