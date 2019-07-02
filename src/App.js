dc8f3400dca9b07aed4289e53e9f4f817366c17a
pekkakyllonen <pkyllone@gmail.com>
2019-07-02 13:44:34 +0300

    https://github.com/pekkakyllonen/my-little-world.git

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+export default () => (
+  <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Welcome To My Hello World!</h1>
+    </header>
+    <p className="App-intro">
+      My name is TravisTest. Welcome, and hello!
+    </p>
+  </div>
+);
