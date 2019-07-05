51a54599ffa7abb969bfd0310bba172ea8d5973b
pekkakyllonen <pkyllone@gmail.com>
2019-07-05 13:38:19 +0300

    Add another line

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default () => (
     <header className="App-header">
       <h1 className="App-title">Welcome To My Little World!</h1>
       <h2>My name is TravisTest. Welcome, and hello!</h2>
+      <h3>Yet another text line</h3>
     </header>
   </div>
 );
